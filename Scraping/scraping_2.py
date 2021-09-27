94d6a2ee8a7420e67e6e1a9e5ede051ca2f4ebf3
hwangstone1 <mylee553@naver.com>
2021-09-27 21:10:44 +0900

    bs4_2

---
+++ Scraping/scraping_2.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/weekday"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+cartoons = soup.find_all("a", attrs={"class":"title"}) # 리스트로 반환
+
+for i in cartoons:
+    print("웹툰:",i.get_text())
