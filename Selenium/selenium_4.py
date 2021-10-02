b06a72d3e6532995beeff21dba974cc7a9b22b10
hwangstone1 <mylee553@naver.com>
2021-10-02 16:47:19 +0900

    exercise_4

---
+++ Selenium/selenium_4.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time, requests
+browser = webdriver.Chrome()
+browser.maximize_window()
+
+# 페이지 이동
+url = "https://play.google.com/store/movies/top"
+browser.get(url)
+
+# 스크롤 내리기
+# 해상도의 높이인 1080 위치로 스크롤 내리기
+# browser.execute_script("window.scrollTo(0, 1080)")
+
+# 화면 가장 아래로 스크롤 내리
+# browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+interval = 2
+
+prev_height = browser.execute_script("return document.body.scrollHeight")
+
+while True:
+    # 스크롤을 가장아래로 내림
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    time.sleep(interval)
+    # 현재 문서 높이를 가져와서 저장
+    curr_height = browser.execute_script("return document.body.scrollHeight")
+    if prev_height == curr_height:
+        break
+
+    prev_height = curr_height
+
+print("스크롤 완료")
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# movies = soup.find_all("div", attrs={"class":["ImZGtf mpg5gc", "Vpfmgd"]})
+# print(len(movies))
+movies = soup.find_all("div", attrs={"class":"Vpfmgd"})
+
+
+# with open("movie.html", "w", encoding="utf8") as f:
+#     f.write(res.text)
+# f.write(soup.prettify())
+
+for movie in movies:
+    text = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+
+    # 할인전 가격
+    o_price = movie.find("span", attrs={"class" : "SUZt4c djCuy"})
+    if o_price:
+        o_price = o_price.get_text()
+    else:
+        #print(text, "<할인 되지 않은 영화>")
+        continue
+    # 할인된 가격
+    price = movie.find("span", attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+
+    # 링크 정보
+
+    link = movie.find("a", attrs={"class":"JC71ub"})["href"]
+    # 올바른 링크 = https://play.google.com + link
+
+    print(f"제목: {text}")
+    print(f"할인 전 가격: {o_price}")
+    print(f"할인 후 가격: {price}")
+    print("링크: ", "https://play.google.com" + link)
+    print("-"*100)
+
+browser.quit()
+
